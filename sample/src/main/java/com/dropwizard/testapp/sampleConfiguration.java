f1fc7024277b9c0b6566e8873495a3ea33703c84
Maksym Bodnar <mmaxbb@gmail.com>
2020-12-03 18:20:13 -0800

    Dropwizard example start

---
+++ sample/src/main/java/com/dropwizard/testapp/sampleConfiguration.java
@@ -0,0 +1,10 @@
+package com.dropwizard.testapp;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class sampleConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
