3f1badd62b4b4879e18ba7a3f046a089f69b2944
Maksym Bodnar <mmaxbb@gmail.com>
2020-12-31 19:28:04 -0800

    Add a test

---
+++ example/src/test/java/com/example/helloworld/db/PhotoDAOTest.java
@@ -0,0 +1,55 @@
+package com.example.helloworld.db;
+
+import com.example.helloworld.core.Photo;
+import io.dropwizard.testing.junit5.DAOTestExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.List;
+import java.util.Optional;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class PhotoDAOTest {
+
+    public DAOTestExtension daoTestRule = DAOTestExtension.newBuilder()
+        .addEntityClass(Photo.class)
+        .build();
+
+    private PhotoDAO photoDAO;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        photoDAO = new PhotoDAO(daoTestRule.getSessionFactory());
+    }
+
+    @Test
+    public void createPhoto() {
+        final Photo me = daoTestRule.inTransaction(() -> photoDAO.create(new Photo("me.jpg", "/path/to/photo", "123abc", LocalDateTime.parse("2020-01-01 10:20:11"))));
+        assertThat(me.getId()).isGreaterThan(0);
+        assertThat(me.getName()).isEqualTo("me.jpg");
+        assertThat(me.getPath()).isEqualTo("/path/to/photo");
+        assertThat(me.getHash()).isEqualTo("123abc");
+        assertThat(photoDAO.findById(me.getId())).isEqualTo(Optional.of(me));
+    }
+
+    @Test
+    public void findAll() {
+        daoTestRule.inTransaction(() -> {
+            photoDAO.create(new Photo("me.jpg", "/path/to/photo1", "123abc", LocalDateTime.parse("2020-01-01")));
+            photoDAO.create(new Photo("kid.jpg", "/path/to/photo2", "432bdf", LocalDateTime.parse("2019-10-10")));
+            photoDAO.create(new Photo("mom.jpg", "/path/to/photo3", "abc432", LocalDateTime.parse("2018-11-01")));
+        });
+
+        final List<Photo> photos = photoDAO.findAll();
+        assertThat(photos).extracting("name").containsOnly("me.jpg", "kid.jpg", "mom.jpg");
+        assertThat(photos).extracting("path").containsOnly("/path/to/photo1", "/path/to/photo2", "/path/to/photo3");
+        assertThat(photos).extracting("hash").containsOnly("123abc", "432bdf", "abc432");
+    }
+}
